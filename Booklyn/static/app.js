4a85cef1a98a2512d9dae319a909ce74e2bb2215
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-10-09 13:44:34 -0700

    remove unecessary files

--- Booklyn/static/app.js
+++
@@ -1,8 +0,0 @@
-//get all the stars
-
-// const one = document.querySelector('[data-star="1"]')
-// const two = document.querySelector('[data-star="2"]')
-// const three = document.querySelector('[data-star="3"]')
-// const four = document.querySelector('[data-star="4"]')
-// const five = document.querySelector('[data-star="5"]')
-
